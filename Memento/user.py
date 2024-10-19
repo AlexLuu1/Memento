1391f62167627fef0e7337abd14973981c9f8bb5
Vrushank Prakash <vprak@berkeley.edu>
2024-10-19 00:21:42 -0700

    added stt

--- Memento/user.py
+++ Memento/user.py
@@ -1,9 +1,17 @@
-import reflex as rx
-from rxconfig import config
-from reflex_audio_capture import AudioRecorderPolyfill, get_codec, strip_codec_part
 from urllib.request import urlopen
 
+import reflex as rx
+
+from reflex_audio_capture import AudioRecorderPolyfill, get_codec, strip_codec_part
+
+import os
+from groq import Groq
+
+# Initialize the Groq client
+client = Groq(api_key="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")
+
 REF = "myaudio"
+
 
 class UserState(rx.State):
     """The app state."""
@@ -21,42 +29,41 @@ class UserState(rx.State):
         if audio_type == "mpeg":
             audio_type = "mp3"
         print(len(chunk), mime_type, codec, audio_type)
-        # with urlopen(strip_codec_part(chunk)) as audio_data:
-        #     try:
-        #         self.processing = True
-        #         yield
-        #         transcription = await client.audio.transcriptions.create(
-        #             model="whisper-1",
-        #             file=("temp." + audio_type, audio_data.read(), mime_type),
-        #         )
-        #     except Exception as e:
-        #         self.has_error = True
-        #         yield capture.stop()
-        #         raise
-        #     finally:
-        #         self.processing = False
-        #     self.transcript.append(transcription.text)
+        with urlopen(strip_codec_part(chunk)) as audio_data:
+            print(type(audio_data))
+            try:
+                self.processing = True
+                yield
+                transcription = await client.audio.transcriptions.create(
+                   file=(("temp." + audio_type, audio_data.read(), mime_type)), # Required audio file
+                   model="whisper-large-v3-turbo", # Required model to use for transcription
+                   prompt="Specify context or spelling",  # Optional
+                   response_format="json",  # Optional
+                   language="en",  # Optional
+                   temperature=0.0  # Optional
+                )
+            except Exception as e:
+                self.has_error = True
+                yield capture.stop()
+                raise
+            finally:
+                self.processing = False
+            self.transcript.append(transcription.text)
 
     def set_timeslice(self, value):
         self.timeslice = value[0]
 
     def set_device_id(self, value):
         self.device_id = value
- 
+        yield capture.stop()
+
     def on_error(self, err):
         print(err)
 
     def on_load(self):
         # We can start the recording immediately when the page loads
         return capture.start()
-    
-    def start_record(self):
-        #vapi.start(assistant_id="c2ecf489-09f7-4f37-9b0b-d106c95e1f2c")
-        return capture.start()
 
-    def stop_record(self):
-        #vapi.stop()
-        return capture.stop()
 
 capture = AudioRecorderPolyfill.create(
     id=REF,
@@ -83,29 +90,6 @@ def input_device_select():
         on_change=UserState.set_device_id,
     )
 
-capture = AudioRecorderPolyfill.create(
-    id=REF,
-    on_data_available=UserState.on_data_available,
-    on_error=UserState.on_error,
-    timeslice=UserState.timeslice,
-    device_id=UserState.device_id,
-    use_mp3=UserState.use_mp3,
-)
-
-def input_device_select():
-    return rx.select.root(
-        rx.select.trigger(placeholder="Select Input Device"),
-        rx.select.content(
-            rx.foreach(
-                capture.media_devices,
-                lambda device: rx.cond(
-                    device.deviceId & device.kind == "audioinput",
-                    rx.select.item(device.label, value=device.deviceId),
-                ),
-            ),
-        ),
-        on_change=UserState.set_device_id,
-    )
 
 def user_index() -> rx.Component:
     return rx.container(
@@ -133,7 +117,7 @@ def user_index() -> rx.Component:
                 rx.button("Stop Recording", on_click=capture.stop()),
                 rx.button(
                     "Start Recording",
-                    on_click=UserState.stop_record(),
+                    on_click=capture.start(),
                 ),
             ),
             rx.card(
@@ -153,3 +137,8 @@ def user_index() -> rx.Component:
         size="1",
         margin_y="2em",
     )
+
+
+# # Add state and page to the app.
+# app = rx.App()
+# app.add_page(index)
